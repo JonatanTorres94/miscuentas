8c08f90fa00d5afdf0c85358c39b6eec301d1822
JonatanTorres94 <79852380+JonatanTorres94@users.noreply.github.com>
2022-05-10 11:46:41 -0300

    funAddMarker

--- app/src/main/java/com/example/miscuentas/view/SuperMarker.kt
+++ app/src/main/java/com/example/miscuentas/view/SuperMarker.kt
@@ -3,6 +3,7 @@ package com.example.miscuentas.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +14,8 @@ import com.example.miscuentas.model.SuperMarkerModel
 import com.example.miscuentas.model.superMarkerProvider
 import com.example.miscuentas.myAdapter.MyAdapterList
 import kotlinx.android.synthetic.main.activity_super_marker.*
+import kotlinx.android.synthetic.main.layout_viewall.view.*
+import kotlinx.android.synthetic.main.layput_diag.view.*
 import com.example.miscuentas.model.superMarkerProvider.Companion as modelSuperMarkerProvider
 
 
@@ -29,39 +32,72 @@ class SuperMarker : AppCompatActivity() {
         val objetSuperMarkProvi = superMarkerProvider
         var total = 0.0
 
+
+        // Boton de agregar compra
         binding.button4.setOnClickListener {
             //Asignacion de valores
             val builder = AlertDialog.Builder(this)
-            val view = layoutInflater.inflate(R.layout.layput_diag,null)
+            val view = layoutInflater.inflate(R.layout.layput_diag, null)
             //creando vista
             builder.setView(view)
             //creando dialog
             val dialog = builder.create()
             dialog.show()
+
+            //Obtener cajas de texto
+            val boxName = view.tv_name_edit.text
+           // val boxPrice = view.tv_price_edit.text
+
+
+            view.button6.setOnClickListener {
+                val boxPriceD = view.tv_price_edit.text.toString()
+                val doublePrice: Double = boxPriceD.toDouble()
+                objetSuperMarkProvi.markerList.add(SuperMarkerModel("$boxName", doublePrice))
+                total += doublePrice
+                Toast.makeText(this, "Producto Agregado correctamente", Toast.LENGTH_SHORT).show()
+
+            }
+
         }
 
 
+        //println(total) ## VER TOTAL POR CONSOLA
 
+        //Boton de ver total
+        binding.button5.setOnClickListener {
+
+
+            val builder = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.layout_viewall, null)
+
+            builder.setView(view)
+            view.tv_total.text = total.toString()
+            val dialog = builder.create()
+            dialog.show()
+
+
+
+        }
 
         objetSuperMarker.markerList.forEach {
             total += it.price
             println(it.nameProduct + " " + it.price)
         }
-       /* ## Agregar un objeto a la lista por codigo
-        objetSuperMarkProvi.markerList.add(SuperMarkerModel("Queso frescoo",5555.55))
-        */
 
-        println(total)
 
     }
 
-    private fun initRecyclerView(){
+
+    private fun initRecyclerView() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerList)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = MyAdapterList(modelSuperMarkerProvider.markerList)
 
-        }
+    }
 
 
 }
+
+
+
 
